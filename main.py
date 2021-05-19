47da77c70d0fa8be92134e85991c592a70a93f81
Miles <git@miles.so>
2021-05-19 12:06:59 +0100

    Added main file

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ from models import example_model
 from scipy.special import softmax
 import numpy as np
 
+def example():
+    model, tokenizer, labels = example_model()
 
-model, tokenizer, labels = example_model()
+    while 1:
+        text = input("sentence>>> ")
+        text = preprocess(text)
+        encoded_input = tokenizer(text, return_tensors='pt')
+        output = model(**encoded_input)
+        scores = output[0][0].detach().numpy()
+        scores = softmax(scores)
 
-while 1:
-    text = input("sentence>>> ")
-    text = preprocess(text)
-    encoded_input = tokenizer(text, return_tensors='pt')
-    output = model(**encoded_input)
-    scores = output[0][0].detach().numpy()
-    scores = softmax(scores)
+        ranking = np.argsort(scores)
+        ranking = ranking[::-1]
+        for i in range(scores.shape[0]):
+            l = labels[ranking[i]]
+            s = scores[ranking[i]]
+            print(f"{i+1}) {l} {np.round(float(s), 4)}")
 
-    ranking = np.argsort(scores)
-    ranking = ranking[::-1]
-    for i in range(scores.shape[0]):
-        l = labels[ranking[i]]
-        s = scores[ranking[i]]
-        print(f"{i+1}) {l} {np.round(float(s), 4)}")
+if __name__ == "__main__":
+    example()
